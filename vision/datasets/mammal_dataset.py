88a98657dfcbc6f978f8579b2631eb84b41b641c
andreistirb <mr.andreistirb@gmail.com>
2019-08-14 18:17:25 +0300

    adding _get_item__ support

--- vision/datasets/mammal_dataset.py
+++ vision/datasets/mammal_dataset.py
@@ -18,6 +18,7 @@ class MammalDataset(Dataset):
         self.categories = {}
         self.images = {}
         self.annotations = defaultdict(list)
+        self.imgs_idx = []
 
         # preprocess json file
         json_file = open(self.annotation_file, "r")
@@ -30,17 +31,47 @@ class MammalDataset(Dataset):
         images = json_data["images"]
         for img in images:
             self.images[img["id"]] = img["file_name"]
+
+        self.imgs_idx = self.images.keys()
         
         annotations = json_data["annotations"]
         for annotation in annotations:
             self.annotations[annotation["image_id"]].append((annotation["bbox"], annotation["category_id"]))
 
     def __get_item__(self, index):
-        return index
+        
+        image_id = self.imgs_idx[index]
+        image_path = self.images[image_id]
+
+        image = cv2.imread(str(image_path))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        annotations = self.annotations[image_id]
+        boxes, labels = map(list, zip(*annotations))
+
+        boxes = self.convert_box(boxes)
+        boxes = np.array(boxes)
+
+        labels = np.array(labels)
+        
+        if self.transform:
+            image, boxes, labels = self.transform(image, boxes, labels)
+        if self.target_transform:
+            boxes, labels = self.target_transform(boxes, labels)
+        return image, boxes, labels
 
     def get_image(self, index):
         return index
 
     def __len__(self):
         return 2
+
+    def convert_box(self, boxes):
+        boxes_return = []
+        for box in boxes:
+            m_box = box
+            m_box[2] = m_box[0] + m_box[2]
+            m_box[3] = m_box[3] + m_box[3]
+            boxes_return.append(m_box)
+        return boxes_return
 
